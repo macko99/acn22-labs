20dbd0d6246712af144f4077453af1442f1ec4e6
Paulo Liedtke <liedtke.paulo@gmail.com>
2022-11-20 00:57:57 +0100

    Add: generate fat-tree in mininet

--- lab3/fat-tree.py
+++ lab3/fat-tree.py
@@ -32,39 +32,98 @@ from mininet.util import waitListening, custom
 import topo
 
 class FattreeNet(Topo):
-	"""
-	Create a fat-tree network in Mininet
-	"""
+    """
+    Create a fat-tree network in Mininet
+    """
 
-	def __init__(self, ft_topo):
-		
-		Topo.__init__(self)
+    def __init__(self, ft_topo):
+        
+        Topo.__init__(self)
 
-		# TODO: please complete the network generation logic here
+        # TODO: please complete the network generation logic here
+        self.servers = ft_topo.servers
+        self.num_ports = ft_topo.num_ports
+        self.core_switches = [switch for switch in ft_topo.switches if switch.type == 'c_sw']
+        self.pod_switches = [switch for switch in ft_topo.switches if switch.type == 'p_sw']
+        modulo_values = [n for n in range(int(self.num_ports / 2))]
+        self.lower_pod_switches = [switch for idx, switch in enumerate(self.pod_switches) if
+                              idx % self.num_ports in modulo_values]
+        self.upper_pod_switches = [switch for switch in self.pod_switches if switch not in self.lower_pod_switches]
+        self.generate()
+
+    def generate(self):
+        core_switches = []
+        upper_switches = []
+        lower_switches = []
+        servers =  []
+
+        #adding switches and servers to mininet
+        for i, core_switch in enumerate(self.core_switches):
+            core_switches.append(self.addSwitch("c_sw{}".format(i), ip=core_switch.id))
+
+        for i, upper_switch in enumerate(self.upper_pod_switches):
+            upper_switches.append(self.addSwitch("u_sw{}".format(i), ip=upper_switch.id))
+                
+        for i, lower_switch in enumerate(self.lower_pod_switches):
+            lower_switches.append(self.addSwitch("l_sw{}".format(i), ip=lower_switch.id))
+
+        for i, server in enumerate(self.servers):
+            servers.append(self.addHost("s{}".format(i), ip=server.id))
+
+
+        linked_edges = []
+        #creating links between core layer and upper layer
+        for core_switch in self.core_switches:
+            for edge in core_switch.edges:
+                pod_switch = edge.rnode
+                core_switch_idx = self.core_switches.index(core_switch)
+                pod_switch_idx = self.upper_pod_switches.index(pod_switch)
+                self.addLink(core_switches[core_switch_idx], upper_switches[pod_switch_idx], bw=15, delay='5ms')
+                linked_edges.append(edge)
+        
+        #creating links upper layer and lower layer
+        for upper_switch in self.upper_pod_switches:
+            for edge in upper_switch.edges:
+                if edge not in linked_edges:
+                    lower_switch = edge.lnode
+                    upper_switch_idx = self.upper_pod_switches.index(upper_switch)
+                    lower_switch_idx = self.lower_pod_switches.index(lower_switch)
+                    self.addLink(upper_switches[upper_switch_idx], lower_switches[lower_switch_idx], bw=15, delay='5ms')
+                    linked_edges.append(edge)
+
+        #creating links between lower layer and servers
+        for server in self.servers:
+            for edge in server.edges:
+                switch = edge.rnode
+                server_idx = self.servers.index(server)
+                switch_idx = self.lower_pod_switches.index(switch)
+                self.addLink(lower_switches[switch_idx], servers[server_idx], bw=15, delay='5ms')
+                linked_edges.append(edge)
 
 
 def make_mininet_instance(graph_topo):
 
-	net_topo = FattreeNet(graph_topo)
-	net = Mininet(topo=net_topo, controller=None, autoSetMacs=True)
-	net.addController('c0', controller=RemoteController, ip="127.0.0.1", port=6653)
-	return net
+    net_topo = FattreeNet(graph_topo)
+    net = Mininet(topo=net_topo, controller=None, autoSetMacs=True)
+    net.addController('c0', controller=RemoteController, ip="127.0.0.1", port=6653)
+    return net
 
 def run(graph_topo):
-	
-	# Run the Mininet CLI with a given topology
-	lg.setLogLevel('info')
-	mininet.clean.cleanup()
-	net = make_mininet_instance(graph_topo)
+    
+    # Run the Mininet CLI with a given topology
+    lg.setLogLevel('info')
+    mininet.clean.cleanup()
+    net = make_mininet_instance(graph_topo)
 
-	info('*** Starting network ***\n')
-	net.start()
-	info('*** Running CLI ***\n')
-	CLI(net)
-	info('*** Stopping network ***\n')
-	net.stop()
+    info('*** Starting network ***\n')
+    net.start()
+    info('*** Running CLI ***\n')
+    CLI(net)
+    info('*** Stopping network ***\n')
+    net.stop()
 
 
 
 ft_topo = topo.Fattree(4)
+
 run(ft_topo)
